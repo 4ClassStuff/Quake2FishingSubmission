1e2d62d84412d93d046383b4384bee70731ae1ed
4ClassStuff <60152699+4ClassStuff@users.noreply.github.com>
2020-05-08 01:59:16 -0400

    Added Money UI

--- game/p_hud.c
+++ game/p_hud.c
@@ -367,6 +367,69 @@ void Cmd_Help_f (edict_t *ent)
 }
 
 
+////////////Display money
+
+void WriteMoney(edict_t *ent)
+{
+	extern int money;
+	char	string[1024];
+	char	*sk;
+
+	if (skill->value == 0)
+		sk = "easy";
+	else if (skill->value == 1)
+		sk = "medium";
+	else if (skill->value == 2)
+		sk = "hard";
+	else
+		sk = "hard+";
+
+	// send the layout
+	Com_sprintf(string, sizeof(string),
+		//"xv 32 yv 8 picn help "			// background
+		"xv 500 yv 400 string2 \"Money: %i\" ",		// money
+		//"xv 0 yv 24 cstring2 \"%s\" "		// level name
+		//"xv 0 yv 54 cstring2 \"%s\" "		// help 1
+		//"xv 0 yv 110 cstring2 \"%s\" "		// help 2
+		//"xv 50 yv 164 string2 \" kills     goals    secrets\" "
+		//"xv 50 yv 172 string2 \"%3i/%3i     %i/%i       %i/%i\" ",
+		money
+		//level.level_name,
+		//game.helpmessage1,
+		//game.helpmessage2,
+		//level.killed_monsters, level.total_monsters,
+		//level.found_goals, level.total_goals,
+		//level.found_secrets, level.total_secrets
+		);
+
+	gi.WriteByte(svc_layout);
+	gi.WriteString(string);
+	gi.unicast(ent, true);
+}
+
+void Cmd_Money_HUD(edict_t *ent){
+
+	if (deathmatch->value)
+	{
+		Cmd_Score_f(ent);
+		return;
+	}
+
+	ent->client->showinventory = false;
+	ent->client->showscores = false;
+
+	if (ent->client->showhelp && (ent->client->pers.game_helpchanged == game.helpchanged))
+	{
+		ent->client->showhelp = false;
+		return;
+	}
+
+	ent->client->showhelp = true;
+	ent->client->pers.helpchanged = 0;
+	WriteMoney(ent);
+}
+
+
 //=======================================================================
 
 /*
